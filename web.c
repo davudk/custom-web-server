369e34baabdee3472bb69575d8e7f96c4259d90d
davudk <davudkap@gmail.com>
2018-02-04 08:26:59 -0500

    Added a basic HTTP response
    Hard coded an http response to see if the browser will catch it

--- web.c
+++ web.c
@@ -6,10 +6,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("requires port argument");
-        return -1;
-    }
+
     printf("creating a socket... ");
     int domain = PF_INET; // IPv4
     int type = SOCK_STREAM; // TCP
@@ -22,7 +19,7 @@ int main(int argc, char *argv[]) {
 
     printf("binding... ");
     addr.sin_family = AF_INET; // IPv4
-    addr.sin_port = htons(atoi(argv[1])); // 80 HTTP
+    addr.sin_port = htons(atoi(argv[1])); // HTTP is 80
     addr.sin_addr.s_addr = htonl(INADDR_ANY); // any local IP
     int status = bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
     printf("(status: %s)\n", status ? "FAILURE" : "SUCCESS");
@@ -34,21 +31,44 @@ int main(int argc, char *argv[]) {
     printf("(status: %s)\n", status ? "FAILURE" : "SUCCESS");
     if (status) return status;
 
+    printf("\n");
     while (1) {
-        struct sockaddr clientAddr; // not used...
         int client_fd = accept(socket_fd, NULL, NULL);
 
-        char buffer[1000];
+        const int BUFFER_SIZE = 70000;
+        char buffer[BUFFER_SIZE];
         int count;
         
-        while (count = recv(client_fd, buffer, 1000, 0)) {
+        char* template = "HTTP/1.0 200 OK\r\n"
+            "Date: Sun, 04 Feb 2018 13:25:32 GMT\r\n"
+            "Expires: -1\r\n"
+            "Cache-Control: private, max-age=0\r\n"
+            "Content-Type: text/html; charset=UTF-8\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "<!doctype html><html><body><pre>\r\n";
+        send(client_fd, template, strlen(template), 0);
+
+        while (count = recv(client_fd, buffer, BUFFER_SIZE, 0)) {
             if (count < 0) {
                 printf("count < 0..");
                 return -1;
             }
 
             printf("%s", buffer);
+
+            send(client_fd, buffer, count, 0);
+
+            // if (count > 65535) { // max limit for HTTP req
+            //     break;
+            // }
+            break;
         }
+        
+        char* end = "</pre></body></html>";
+        send(client_fd, end, strlen(end), 0);
+
+        close(client_fd);
     }
 
     close(socket_fd);
